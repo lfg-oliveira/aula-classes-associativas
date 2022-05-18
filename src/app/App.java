46a042f4dd299a44ab2eadbe6c98e7702fe61bbf
android <android@LABFAE.local>
2022-05-18 20:31:39 -0300

    nao quero perder o codigo

--- src/app/App.java
+++ src/app/App.java
@@ -46,26 +46,27 @@ public class App {
                 }while(op2 != 6);
                 break;
                 case 2:
-                do{
-                System.out.println("\n\n===[Pessoa]===");
-                System.out.println("1 - Cadastrar");
-                System.out.println("2 - Alterar");
-                System.out.println("3 - Excluir");
-                System.out.println("4 - Consultar");
-                System.out.println("5 - Imprimir");
-                System.out.println("6 - Voltar ao menu principal");
-                System.out.println("Opção: ");
-                op2 = leitor.nextInt();
-                switch (op2) {
-                    case 1:
+                    do{
+                        System.out.println("\n\n===[Pessoa]===");
+                        System.out.println("1 - Cadastrar");
+                        System.out.println("2 - Alterar");
+                        System.out.println("3 - Excluir");
+                        System.out.println("4 - Consultar");
+                        System.out.println("5 - Imprimir");
+                        System.out.println("6 - Voltar ao menu principal");
+                        System.out.println("Opção: ");
+                        op2 = leitor.nextInt();
+                        switch (op2) {
+                            case 1:
+                                
+                                break;
                         
-                        break;
-                
-                    default:
-                        break;
-                }
-                }while(op2 != 6);
-                break;
+                            default:
+
+                                break;
+                        }
+                    }while(op2 != 6);
+                    break;
                 default:
                     break;
             }
